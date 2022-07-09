d78247f9fb55bd249e0a6f870ac3e05c6fc7b67e
mojorel <53398006+mojorel@users.noreply.github.com>
2022-07-09 17:26:35 +0800

    Initial Commit

---
+++ src/test/java/com/mojorel/thingstodo/ThingsToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mojorel.thingstodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThingsToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
